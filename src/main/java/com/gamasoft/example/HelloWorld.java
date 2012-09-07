589fa43744b2e8054abc6d2d9ecb7b08dd8c0e7c
Uberto Barbini <uberto.gama@gmail.com>
2012-09-07 16:08:22 +0100

    add tests jmeter

--- src/main/java/com/gamasoft/example/HelloWorld.java
+++ src/main/java/com/gamasoft/example/HelloWorld.java
@@ -21,6 +21,7 @@ public class HelloWorld extends AbstractHandler
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
         response.getWriter().println("<h1>Hello World</h1>");
+        response.getWriter().println("<p>thread name " + Thread.currentThread().getName() + "</p>");
     }
 
     public static void main(String[] args) throws Exception
