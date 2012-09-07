ec444f2b504a04d2d559f1bc72dcf23a56b5a4ff
Uberto Barbini <uberto.gama@gmail.com>
2012-09-07 15:32:31 +0100

    copy from http://wiki.eclipse.org/Jetty/Tutorial/Jetty_and_Maven_HelloWorld

---
+++ src/main/java/com/gamasoft/example/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.gamasoft.example;
+
+import org.eclipse.jetty.server.Request;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class HelloWorld extends AbstractHandler
+{
+    public void handle(String target,
+                       Request baseRequest,
+                       HttpServletRequest request,
+                       HttpServletResponse response)
+            throws IOException, ServletException
+    {
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+        baseRequest.setHandled(true);
+        response.getWriter().println("<h1>Hello World</h1>");
+    }
+
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server(8080);
+        server.setHandler(new HelloWorld());
+
+        server.start();
+        server.join();
+    }
+}
