3a28a2c56a0b28a93030131f4d46007e5d01f7d3
Uberto Barbini <uberto.gama@gmail.com>
2012-09-11 15:54:02 +0100

    better test

--- src/test/java/com/gamasoft/example/model/ExchangeTest.java
+++ src/test/java/com/gamasoft/example/model/ExchangeTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 public class ExchangeTest {
@@ -42,11 +43,15 @@ public class ExchangeTest {
         Bid b1 = exchange.buy(traderA, myStock, 10);
         Bid b2 = exchange.sell(traderB, myStock, 12);
 
+        assertThat(b2.getId(), not(b1.getId()));
+
         assertThat(exchange.getTransactions().size(), is( 0));
 
         Bid b3 = exchange.sell(traderB, myStock, 9);
         assertThat(exchange.getTransactions().size(), is( 1));
         assertThat(exchange.getTransactions().get(0), is(new Transaction(b1, b3, 9)));
+
+        assertThat(exchange.getTransactions().get(0).toString(), is("Transaction{buy=Bid{id=90401895, trader=Trader{name='Al'}, stock=Stock{name='AB Corp', ticker='ABC'}, price=10.0}, sell=Bid{id=783977366, trader=Trader{name='Ben'}, stock=Stock{name='AB Corp', ticker='ABC'}, price=9.0}, price=9.0}"));
 
 
     }
